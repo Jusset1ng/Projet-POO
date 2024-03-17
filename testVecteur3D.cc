127024bd01f12925acca633262b8ca865d0e18cf
Jusset1ng <161937523+Jusset1ng@users.noreply.github.com>
2024-03-17 19:59:07 +0100

    Update testVecteur3D.cc

--- testVecteur3D.cc
+++ testVecteur3D.cc
@@ -6,13 +6,42 @@ using namespace std;
 
 int main()
 {
-Vecteur3D vect1;
-Vecteur3D vect2(1.1,2.2,3.3);
-Vecteur3D vect3(vect2);
+// un vecteur en 3D :
+Vecteur3D vect1(1.0, 2.0, -0.1);
 
-if(vect2==vect3){
-	vect3=vect3+vect2;
-cout<<vect3<<endl;}
+// un autre vecteur en 3D
+Vecteur3D vect2(2.6, 3.5,  4.1);
+
+Vecteur3D vect3(vect1);  // copie de V1
+Vecteur3D vect4;         // le vecteur nul
+
+cout << "Vecteur 1 : " << vect1 << endl;
+cout << "Vecteur 2 : " << vect2 << endl;
+cout << "Vecteur 3 : " << vect3 << endl;
+cout << "Vecteur 4 : " << vect4 << endl;
+
+cout << "Le vecteur 1 est ";
+if (vect1 == vect2) {
+    cout << "égal au";
+} else {
+    cout << "différent du";
+}
+cout << " vecteur 2," << endl << "et est ";
+if (vect1 != vect3) {
+    cout << "différent du";
+} else {
+    cout << "égal au";
+}
+cout << " vecteur 3." << endl;
+
+cout << (vect1 ^ vect2) << endl;
+
+Vecteur3D direction;
+direction = ~vect3;
+cout << direction;
+
 return 0;
+
+
 }
 
