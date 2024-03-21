fdf00cf2c509c2902c958cfdc4da2a88e082f83c
EmmanuelPonti <162562340+EmmanuelPonti@users.noreply.github.com>
2024-03-21 12:52:14 +0100

    Add files via upload

---
+++ enceinte.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Enceinte{
+	Enceinte (double a=20.0, double b=20., double c=20.0):
+	hauteur(a),largeur(b),profondeur(c)
+	{}
+	double hauteur;
+	double largeur;
+	double profondeur;
+	
+	};
+	
+	int main(){
+		return 0;}
