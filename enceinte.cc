06f1c61ff5573d536733757319bf8da5106851f9
Jusset1ng <161937523+Jusset1ng@users.noreply.github.com>
2024-03-22 02:25:06 +0100

    Update enceinte.cc

--- enceinte.cc
+++ enceinte.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Enceinte{
-	Enceinte (double a=20.0, double b=20., double c=20.0):
+	Enceinte (double a=20.0, double b=20.0, double c=20.0):
 	hauteur(a),largeur(b),profondeur(c)
 	{}
 	double hauteur;
