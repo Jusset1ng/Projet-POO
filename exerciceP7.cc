2780d82f13a2b124a05d645e7dfa10bc388d7ed9
EmmanuelPonti <162562340+EmmanuelPonti@users.noreply.github.com>
2024-04-04 16:31:49 +0200

    Update exerciceP7.cc

--- exerciceP7.cc
+++ exerciceP7.cc
@@ -75,7 +75,7 @@ int main() {
 S.ajouter_particule(ptr1);
 S.ajouter_particule(ptr2);
 
-T.dessine(S);//devrait être S.dessine_sur(T); une fois la méthode dessine_sur mise dans les classes dessinables
+T.dessine(S);//devrait être S.dessine_sur(T); une fois la méthode dessine_sur mise dans les classes dessinables(question!!)
 
     return 0;
 }
