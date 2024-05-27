bbc137405a5ca45a469aac3082d842f46f8699d3
EmmanuelPonti <162562340+EmmanuelPonti@users.noreply.github.com>
2024-05-27 23:32:14 +0200

    Delete exercice_P11 directory

--- exercice_P11/Qt_GL/fragment_shader.glsl
+++
@@ -1,6 +0,0 @@
-varying vec4 ma_couleur; // Maintenant, c'est un vec4 avec la composante alpha pour l'opacité
-
-void main()
-{
-  gl_FragColor = ma_couleur; // L'opacité est maintenant incluse dans ma_couleur
-}
